4564d996b73a79925157cade6f0cb616b8370bb9
sushantnegee <sushantnegee@gmail.com>
2023-04-07 02:08:02 +0530

    user schema and register/ login route created not tested yet

---
+++ server/controllers/userController.js
@@ -0,0 +1,69 @@
+const User = require("../models/userModels");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const registerUser = async (req, res) => {
+  const { name, email, password } = req.body;
+  const user = await User.findOne({ name });
+
+  if (user) {
+    return res.status(400).json({ message: "User Already Exists" });
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const newUser = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  if (newUser) {
+    const token = jwt.sign({ userId: newUser._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+    res.json({
+      _id: newUser._id,
+      name: newUser.name,
+      email: newUser.email,
+      pic: newUser.pic,
+      token: token,
+    });
+  } else {
+    throw new Error("Failed to Create account");
+  }
+};
+
+const authUser = async(req,res)=>{
+const {email,password}  = req.body;
+try {
+    const user = await User.findOne({email});
+    if(!user){
+        res.status(401)
+        throw new Error('Invalid email or passswords')
+    }
+
+    const validPassword = await bcrypt.compare(password,user.password);
+    if(!validPassword){
+        res.status(400)
+        .json({message:'Invalid Credentials'})
+    }
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+        expiresIn: "1h",
+      });
+      console.log(token) 
+      res.status(201).json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        token: token, 
+      });
+
+} catch (error) {
+    res.status(500).json({ message: error.message });
+}
+
+}
+
+module.exports = { registerUser, authUser };
--- server/server.js
+++ server/server.js
@@ -1,11 +1,15 @@
 const express = require("express")
 const dotenv = require("dotenv");
 const connectDataBase = require("./config/database");
+const cors = require('cors')
+const userRoutes= require("./routes/userRoutes")
 
 const app = express();
+app.use(cors())
 
 dotenv.config();
 app.use(express.json());
+app.use('/user',userRoutes);
 
 connectDataBase();
 app.get("/", (req, res) => {
