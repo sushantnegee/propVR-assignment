756c8afab6399b0fc3cc12a5822b860dc67e70ba
sushantnegee <sushantnegee@gmail.com>
2023-04-06 22:01:02 +0530

    login signup ui created

---
+++ client/src/Components/Authentication/Login.jsx
@@ -0,0 +1,38 @@
+import { Button, FormControl, FormLabel, Input, InputGroup, InputRightElement, VStack } from '@chakra-ui/react'
+import React, { useState } from 'react'
+import {BiHide,BiShow } from 'react-icons/bi'
+import './Auth.css';
+
+const Login = () => {
+    const [loading,setLoading] = useState(false);
+    const [email,setEmail] = useState();
+    const [password,setPassword] = useState();
+    const [show,setShow] = useState();
+
+    const submitHandler = ()=>{
+
+    }
+  return (
+    <VStack>
+        <FormControl>
+  <FormLabel>Email address</FormLabel>
+  <Input type='email' onChange={(e)=>setEmail(e.target.value)} />
+</FormControl>
+<FormControl>
+  <FormLabel>Password</FormLabel>
+  <InputGroup>
+  <Input type={show?"text":"password"} onChange={(e)=>setPassword (e.target.value)} />
+  <InputRightElement>
+  <Button size={'sm'} h={'70%'} onClick={()=>setShow(!show)}>
+    {show?<BiHide/>:<BiShow/>}
+  </Button>
+  </InputRightElement>
+  </InputGroup>
+</FormControl>
+<Button className='submitButton' style={{marginTop:"30px"}} isLoading={loading} onClick={submitHandler} colorScheme='orange' w={'100% '} >Login</Button>
+
+    </VStack>
+  )
+}
+
+export default Login
---
+++ client/src/Pages/HomePage.jsx
@@ -0,0 +1,44 @@
+import {
+  Box,
+  Container,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+} from "@chakra-ui/react";
+import React from "react";
+import Login from "../Components/Authentication/Login";
+import Signup from "../Components/Authentication/Signup";
+
+const HomePage = () => {
+  return (
+    <Container  maxW={"xl"} centerContent>
+      <Box
+        padding={3}
+        width={"100%"}
+        m={"50px 0px 80px 0px"}
+        p={4}
+        borderRadius={"lg"}
+        borderWidth={"1px"}
+      >
+        <Tabs variant="soft-rounded" colorScheme="orange">
+          <TabList>
+            <Tab w={"50%"}>Login</Tab>
+            <Tab w={"50%"}>Signup</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Login />
+            </TabPanel>
+            <TabPanel>
+              <Signup/>
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
+  );
+};
+
+export default HomePage;
